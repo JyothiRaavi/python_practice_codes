27689813a47df0704a68584035e45e55e958ba3b
JyothiRaavi <jyothiraavi345@gmail.com>
2025-01-09 10:46:53 +0530

    Initial commit

---
+++ VariablesDataTypes/Overview.py
@@ -0,0 +1,4 @@
+#1.python is dynamically typed programming language.
+#2. In python while declaring variables we no need to mention the datatypes.
+#3.Datatype will be assigned by language based on values stored in the variables.(at run time the datatype will be assigned to the variables)
+#the bytecode extension in python is ".pyc"
---
+++ VariablesDataTypes/Variables.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+name = "jyothi"
+print("Addition of a and b is : ", a+b)
+#in python everything is object
+print(f'Hello, {name}!')
